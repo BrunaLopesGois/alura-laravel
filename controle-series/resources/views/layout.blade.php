0f2a0eb1023dd5ec6eef491938bf282cb05baf87
Bruna Lopes <bruna-chan@live.com>
2021-01-05 18:25:41 -0300

    commit

---
+++ controle-series/resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Séries</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron mt-2">
+            <h1>@yield('cabecalho')</h1>
+
+        @yield('conteudo')
+        </div>
+    </div>
+</body>
+</html>
